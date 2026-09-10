eb7c96f670402a2fe2d46d81dd3378951899e74f
tengismq19 <tengis.chadraabal@students.mq.edu.au>
2026-09-10 23:22:41 +1000

    Remove unused burn and restore timers from tree

--- source/Tree.java
+++ source/Tree.java
@@ -1,9 +1,7 @@
-// Represents a tree and controls its current state.
+// Creating the states for tree
 public class Tree {
 
     private TreeState state;
-    private int burnTime;
-    private int restoreTime;
 
     public Tree() {
         state = TreeState.GREEN;
@@ -35,13 +33,5 @@ public class Tree {
 
     public TreeState getState() {
         return state;
-    }
-
-    public int getBurnTime() {
-        return burnTime;
-    }
-
-    public int getRestoreTime() {
-        return restoreTime;
     }
 }
