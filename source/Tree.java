cd2863bde09e5747a6607076f5fda2eaf8c170e2
tengismq19 <tengis.chadraabal@students.mq.edu.au>
2026-09-10 23:02:06 +1000

    Add tree state checks and prevent invalid transitions

--- source/Tree.java
+++ source/Tree.java
@@ -1,5 +1,6 @@
-//Creating the states for tree
+// Represents a tree and controls its current state.
 public class Tree {
+
     private TreeState state;
     private int burnTime;
     private int restoreTime;
@@ -9,11 +10,27 @@ public class Tree {
     }
 
     public void burn() {
-        state = TreeState.BURNING;
+        if (isGreen()) {
+            state = TreeState.BURNING;
+        }
     }
 
     public void extinguish() {
-        state = TreeState.BURNED;
+        if (isBurning()) {
+            state = TreeState.BURNED;
+        }
+    }
+
+    public boolean isGreen() {
+        return state == TreeState.GREEN;
+    }
+
+    public boolean isBurning() {
+        return state == TreeState.BURNING;
+    }
+
+    public boolean isBurned() {
+        return state == TreeState.BURNED;
     }
 
     public TreeState getState() {
